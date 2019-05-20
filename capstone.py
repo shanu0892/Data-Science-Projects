4f079506907274e72f0e9556851d1dcd47c6f2ad
shanu0892 <shanu.subbu@gmail.com>
2019-05-20 14:14:23 +1000

    Notebook for Capstone project

---
+++ capstone.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook is used for Capstone project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
